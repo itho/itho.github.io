e6180637fd902a26a1411d6b69be47ffe1d76d15
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-01-15 19:40:00 +0000

    Added existing projects and images to the new repo

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,11 +9,11 @@
           <v-card-text>
             <p>We love to use the latest technologies to create the most beautiful and responsive web and mobile applications.</p>
             <p class="showcase">
-              <a href="https://developer.mozilla.org/en/docs/Web/Guide/HTML/HTML5"><img src="http://itho.co/assets/img/vendor/html5.png"/></a>
+              <a href="https://developer.mozilla.org/en/docs/Web/Guide/HTML/HTML5"><img src="/static/img/logo-html5.png"/></a>
               <a href="https://laravel.com/"><img src="https://camo.githubusercontent.com/5ceadc94fd40688144b193fd8ece2b805d79ca9b/68747470733a2f2f6c61726176656c2e636f6d2f6173736574732f696d672f636f6d706f6e656e74732f6c6f676f2d6c61726176656c2e737667"/></a>
               <a href="https://nodejs.org/en/"><img src="https://camo.githubusercontent.com/9c24355bb3afbff914503b663ade7beb341079fa/68747470733a2f2f6e6f64656a732e6f72672f7374617469632f696d616765732f6c6f676f2d6c696768742e737667"/></a>
               <a href="https://vuejs.org/"><img src="https://camo.githubusercontent.com/728ce9f78c3139e76fa69925ad7cc502e32795d2/68747470733a2f2f7675656a732e6f72672f696d616765732f6c6f676f2e706e67"/></a>
-              <a href="https://facebook.github.io/react-native/"><img src="http://itho.co/assets/img/vendor/react-native.png"/></a>
+              <a href="https://facebook.github.io/react-native/"><img src="/static/img/logo-react-native.png"/></a>
             </p>
           </v-card-text>
         </v-card>
