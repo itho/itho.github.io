a5eeeeab329d110ae97bb60ff0870753865dcff6
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-01-17 10:15:19 +0000

    Updated fontawesome usage to Vuetify standard

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -11,20 +11,20 @@
             Please get in contact if you would like to discuss any opportunities.</p>
             <p>Email: <a href="mailto:contact@itho.co">contact@itho.co</a></p>
             
-            <v-btn icon id="btn-twitter" href="https://twitter.com/ieuauauauauauan">
-              <v-icon class="fab fa-twitter"></v-icon>
+            <v-btn id="twitter" icon href="https://twitter.com/ieuauauauauauan">
+              <v-icon>fa-twitter</v-icon>
             </v-btn>
-            <v-btn icon id="btn-linkedin" href="https://www.linkedin.com/in/ieuan-thomas-187a5576">
-              <v-icon class="fab fa-linkedin"></v-icon>
+            <v-btn id="linkedin" icon href="https://www.linkedin.com/in/ieuan-thomas-187a5576">
+              <v-icon>fa-linkedin</v-icon>
             </v-btn>
-            <v-btn icon id="btn-facebook" href="https://www.facebook.com/ieuan">
-              <v-icon class="fab fa-facebook"></v-icon>
+            <v-btn id="facebook" icon href="https://www.facebook.com/ieuan">
+              <v-icon>fa-facebook</v-icon>
             </v-btn>
-            <v-btn icon id="btn-google-plus" href="https://plus.google.com/u/0/111160770619114139671">
-              <v-icon class="fab fa-google-plus"></v-icon>
+            <v-btn id="google-plus" icon href="https://plus.google.com/u/0/111160770619114139671">
+              <v-icon>fa-google-plus</v-icon>
             </v-btn>
-            <v-btn icon id="btn-github" href="https://github.com/itho/">
-              <v-icon class="fab fa-github"></v-icon>
+            <v-btn id="github" icon href="https://github.com/itho/">
+              <v-icon>fa-github</v-icon>
             </v-btn>
           </v-card-text>
         </v-card>
@@ -34,15 +34,12 @@
 </template>
 
 <style scoped>
-  .fab {
-    font-size: 24pt;
-    padding-left: 10px;
-    padding-right: 10px;
+  .btn {
     color: slategrey !important;
   }
-  #btn-twitter:hover > div > i { color: #1da1f2 !important; }
-  #btn-linkedin:hover > div > i { color: #007bb6 !important; }
-  #btn-facebook:hover > div > i { color: #3b5998 !important; }
-  #btn-google-plus:hover > div > i { color: #d34836 !important; }
-  #btn-github:hover > div > i { color: #000000 !important; }
+  #twitter:hover > div > i { color: #1da1f2 !important; }
+  #linkedin:hover > div > i { color: #007bb6 !important; }
+  #facebook:hover > div > i { color: #3b5998 !important; }
+  #google-plus:hover > div > i { color: #d34836 !important; }
+  #github:hover > div > i { color: #000000 !important; }
 </style>
