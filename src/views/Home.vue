4dc458fb898214c6409382881da10cb475a3a23e
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-02-20 22:41:50 +0000

    New build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@
     data () {
       return {
         items: [
-          { title: 'Modern Designas', icon: 'color_lens', text: "Utilising modern and elegant component frameworks, we're able to build you the highest quality interfaces, regardless of your user's devices. Experienced in Google's Material Design philosophy as well as Twitter's Bootstrap web UI framework and many others." },
+          { title: 'Modern Designs', icon: 'color_lens', text: "Utilising modern and elegant component frameworks, we're able to build you the highest quality interfaces, regardless of your user's devices. Experienced in Google's Material Design philosophy as well as Twitter's Bootstrap web UI framework and many others." },
           { title: 'Fast development', icon: 'flash_on', text: "Personal contact time with our devs means that you're able to easily communicate exactly what it is you like, want and need. We're perfectionists also, so every project will be taken care of to the highest standard and in a time frame that meets your needs." },
           { title: 'Completely Customisable', icon: 'build', text: "It's your website, so it should look and feel exaclty how you want it to. Every aspect of the final result will be able to be tweaked just to your liking, and we'll ensure that the sites feel great to the touch." }
         ]
