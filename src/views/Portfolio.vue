d2e62290cd0266b4f5ce14308aa9c1e975b150e9
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-03-09 18:49:28 +0000

    Updated theme

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -7,7 +7,16 @@
             <div class="headline">Projects</div>
           </v-card-title>
           <v-card-text class="pb-5">
-            Please check back in the future to see updates on our projects.
+            <v-container fluid>
+              <v-layout row wrap>
+                <v-flex md4><img src="https://cryptotracker.itho.co/static/images/launcher-icon-192.png"/></v-flex>
+                <v-flex md8 text-xs-left>
+                  <div class="headline">CryptoTracker</div>
+                  <a href="https://cryptotracker.itho.co">cryptotracker.itho.co</a>
+                  <div><br>CryptoTracker is an asset management tool for cryptocurrencies written in Vue.js and using Google Firebase as an authenication and database platform.</div>
+                </v-flex>
+              </v-layout>
+            </v-container>
           </v-card-text>
         </v-card>
       </v-flex>
