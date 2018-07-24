b50cb50131b0e78d5552fa60868b4621aeb13287
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-07-24 09:58:18 -0400

    added portfolio section

---
+++ src/components/Games.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="games" class="section">
+    <div class="container">
+      <h1 class="title has-text-light">{{ title }}</h1>
+      <h2 class="subtitle has-text-light" v-html="description"></h2>
+      <card-layout :items="games"/>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import CardLayout from '@/components/CardLayout.vue';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+    CardLayout,
+  },
+})
+export default class Games extends Vue {
+  private title: string = 'Games';
+  private description: string = 'Web games written in <strong style="color: #f953c6;">JavaScript</strong> & mobile games in <strong style="color: #f953c6;">C#</strong> with Unity.';
+  private games: any = [
+    {
+      title: 'Aurora',
+      type: 'Android / iOS',
+      description: 'Coming soon, this game is a challenging journey through the beauty of colour...',
+      imageUrl: null,
+      url: null,
+    },
+  ];
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/components/WebApps.vue
+++ src/components/WebApps.vue
@@ -1,26 +1,57 @@
 <template>
-  <section class="hero is-medium">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Web Apps
-        </h1>
-        <h2 class="subtitle">
-          Hero subtitle
-        </h2>
-      </div>
+  <section id="webapps" class="section">
+    <div class="container">
+      <h1 class="title has-text-light">{{ title }}</h1>
+      <h2 class="subtitle has-text-light" v-html="description"></h2>
+      <card-layout :items="webapps"/>
     </div>
   </section>
 </template>
 
 <script lang="ts">
+import CardLayout from '@/components/CardLayout.vue';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component
-export default class BrandBanner extends Vue {}
+@Component({
+  components: {
+    CardLayout,
+  },
+})
+export default class WebApps extends Vue {
+  private title: string = 'Web Apps';
+  private description: string = 'A range of simple utilities to complex proof of concepts & progressive web apps.';
+  private webapps: any = [
+    {
+      title: 'Around',
+      type: 'Web / Mobile (PWA)',
+      description: 'A truly social network for discovering what\'s around you.',
+      imageUrl: 'https://around.itho.co/static/img/icons/android-chrome-192x192.png',
+      url: 'https://around.itho.co',
+    },
+    {
+      title: 'CryptoTracker',
+      type: 'Web / Mobile (PWA)',
+      description: 'A Vue.js project to track and manage crypto investments.',
+      imageUrl: 'https://cryptotracker.itho.co/static/images/launcher-icon-192.png',
+      url: 'https://cryptotracker.itho.co',
+    },
+    {
+      title: 'HackerRank.js',
+      type: 'Web / Local',
+      description: 'A JavaScript framework for running HackerRank challenges on your local machine.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/65/HackerRank_logo.png',
+      url: 'https://github.com/itho/hackerrank.js',
+    },
+    {
+      title: 'MEVN-Boilerplate',
+      type: 'Web / Local',
+      description: 'MongoDB, Express, Vue.js & Node.js boilerplate for bootstrapping PWA development.',
+      imageUrl: 'https://itho.co/static/img/mevn-boilerplate.png',
+      url: 'https://github.com/itho/mevn-boilerplate',
+    },
+  ];
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
