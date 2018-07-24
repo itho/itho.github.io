b667f19e4159bb0e8a55966f54a2f0bb5d2a7a16
Ieuan Thomas <ieuan.thomas@gmail.com>
2018-07-23 23:21:22 -0400

    rebuild with vue-cli 3

---
+++ src/components/WebApps.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Web Apps
+        </h1>
+        <h2 class="subtitle">
+          Hero subtitle
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BrandBanner extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,23 @@
 <template>
-  <v-container grid-list-md>
-    <v-layout row wrap text-xs-center>
-      <v-flex xs12 md4 v-for="(item, i) in items" :key="i">
-        <v-card class="pa-3" style="height: 100%;">
-          <v-card-text class="text-xs-center">
-            <v-icon v-html="item.icon" x-large class="blue--text text--lighten-2"></v-icon>
-          </v-card-text>
-          <v-card-title primary-title class="layout justify-center">
-            <div v-text="item.title" class="headline text-xs-center"></div>
-          </v-card-title>
-          <v-card-text v-text="item.text"></v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="home">
+    <!-- <BrandBanner/> -->
+    <WebApps/>
+    <img src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
-<script>
-  export default {
-    data () {
-      return {
-        items: [
-          { title: 'Modern Designs', icon: 'color_lens', text: "Utilising modern and elegant component frameworks, we're able to build you the highest quality interfaces, regardless of your user's devices. Experienced in Google's Material Design philosophy as well as Twitter's Bootstrap web UI framework and many others." },
-          { title: 'Fast development', icon: 'flash_on', text: "Personal contact time with our devs means that you're able to easily communicate exactly what it is you like, want and need. We're perfectionists also, so every project will be taken care of to the highest standard and in a time frame that meets your needs." },
-          { title: 'Completely Customisable', icon: 'build', text: "It's your website, so it should look and feel exaclty how you want it to. Every aspect of the final result will be able to be tweaked just to your liking, and we'll ensure that the sites feel great to the touch." }
-        ]
-      }
-    }
-  }
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import BrandBanner from '@/components/BrandBanner.vue';
+import WebApps from '@/components/WebApps.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+    WebApps,
+  },
+})
+export default class Home extends Vue {}
 </script>
